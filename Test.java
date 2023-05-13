391dd71172998accdfde07982d9379080d301b5b
Neeraj Gupta <124443763+neeerajgupta@users.noreply.github.com>
2023-05-13 23:06:47 +0530

    Update and rename file2.txt to Test.java
    java coded

---
+++ Test.java
@@ -0,0 +1,16 @@
+//--------------------------------------------java first program-----------------------------------------
+//import java.util.Scanner;
+
+//import java.util.Scanner; 
+
+public class Test{ 
+ 
+public static void main(String[] args){ 
+		//Scanner sc=new Scanner(System.in);  
+		
+	
+	
+	System.out.println("hello");
+		  
+}
+}
